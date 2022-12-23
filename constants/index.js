e9488a9b1774d9c11608fb510c2460de52f14f9b
emirmorgan <aemirdnr@gmail.com>
2022-12-23 08:24:49 +0300

    Update: Constants

---
+++ constants/index.js
@@ -0,0 +1,75 @@
+export const projects = [
+  {
+    title: "Next.js Portfolio [This Website]",
+    source: "https://github.com/emirmorgan",
+    live: "https://github.com/emirmorgan",
+    tags: ["Next.js", "react-three-fiber", "Tailwindcss"],
+    imgSrc: "https://source.unsplash.com/random",
+  },
+  {
+    title: "Next.js Twitter Clone",
+    source: "https://github.com/emirmorgan/nextjs-twitter-clone",
+    live: "https://nextjs-twitter.netlify.app/",
+    tags: [
+      "Next.js",
+      "Redux",
+      "Typescript",
+      "Formik / Yup",
+      "Tailwindcss",
+      "Firebase(Firestore & Auth)",
+    ],
+    imgSrc: "https://source.unsplash.com/random",
+  },
+  {
+    title: "React Instagram Clone",
+    source: "https://github.com/emirmorgan/react-instagram-clone",
+    live: "https://reactjs-ig-clone.netlify.app/",
+    tags: [
+      "React.js",
+      "Redux",
+      "Typescript",
+      "Formik / Yup",
+      "Tailwindcss",
+      "Firebase(Firestore & Auth)",
+      "SASS",
+    ],
+    imgSrc: "https://source.unsplash.com/random",
+  },
+  {
+    title: "Next.js Metaverse Page",
+    source: "https://github.com/emirmorgan/nextjs-metaverse-page",
+    live: "nextjs-metaverse.netlify.app",
+    tags: ["Next.js", "Typescript", "Framer Motion", "Tailwindcss"],
+    imgSrc: "https://source.unsplash.com/random",
+  },
+  {
+    title: "React Spotify Clone",
+    source: "https://github.com/emirmorgan/react-spotify-clone",
+    live: "imaginative-otter-9b6660.netlify.app",
+    tags: ["React.js", "Redux", "Tailwindcss", "React Router"],
+    imgSrc: "https://source.unsplash.com/random",
+  },
+  {
+    title: "GraphQL Rick and Morty Page",
+    source: "https://github.com/emirmorgan/graphql-rick-and-morty",
+    live: "https://ubiquitous-valkyrie-169baa.netlify.app/",
+    tags: [
+      "React.js",
+      "Redux",
+      "GraphQL",
+      "Apollo",
+      "Material UI",
+      "Tailwindcss",
+    ],
+    imgSrc: "https://source.unsplash.com/random",
+  },
+];
+
+export const socials = [
+  {
+    icon: "github",
+    link: "https://github.com/emirmorgan",
+  },
+  { icon: "linkedin", link: "https://linkedin.com/in/emirmorgan" },
+  { icon: "twitter", link: "https://twitter.com/emirmrg" },
+];
