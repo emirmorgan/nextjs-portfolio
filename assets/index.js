32f29a488bca14ccfcae5202786baca6d60bc4ba
emirmorgan <aemirdnr@gmail.com>
2022-12-23 08:20:28 +0300

    Update: Assets

---
+++ assets/index.js
@@ -0,0 +1,294 @@
+const TopPattern = () => {
+  return (
+    <svg
+      width="100"
+      height="485"
+      viewBox="0 0 100 485"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="50" cy="3" r="2.5" fill="black" stroke="black" />
+      <path
+        d="M50 3L57.1096 17.6058C83.8835 72.6092 81.212 137.388 50 190V190"
+        stroke="black"
+      />
+      <path
+        d="M50 190L42.8904 204.606C16.1165 259.609 18.788 324.388 50 377V377"
+        stroke="black"
+      />
+      <circle cx="50" cy="190" r="2.5" fill="white" stroke="black" />
+      <circle cx="50" cy="377" r="2.5" fill="black" stroke="black" />
+      <path d="M50 377V430" stroke="black" />
+      <path d="M50 479V485" stroke="black" />
+      <path d="M50 435V441" stroke="black" />
+      <path d="M50 446V452" stroke="black" />
+      <path d="M50 457V463" stroke="black" />
+      <path d="M50 468V474" stroke="black" />
+    </svg>
+  );
+};
+
+const MiddlePattern = () => {
+  return (
+    <svg
+      width="100"
+      height="400"
+      viewBox="0 0 100 400"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="50" cy="200" r="6" fill="black" />
+      <circle cx="50" cy="184" r="2.5" stroke="black" />
+      <circle cx="50" cy="216" r="2.5" stroke="black" />
+      <line x1="49.5" y1="174" x2="49.5" y2="75" stroke="black" />
+      <line x1="49.5" y1="325" x2="49.5" y2="226" stroke="black" />
+      <line x1="49.5" y1="72" x2="49.5" y2="64" stroke="black" />
+      <line x1="49.5" y1="61" x2="49.5" y2="53" stroke="black" />
+      <line x1="49.5" y1="50" x2="49.5" y2="42" stroke="black" />
+      <line x1="49.5" y1="39" x2="49.5" y2="31" stroke="black" />
+      <line x1="49.5" y1="28" x2="49.5" y2="20" stroke="black" />
+      <line x1="49.5" y1="380" x2="49.5" y2="372" stroke="black" />
+      <line x1="49.5" y1="369" x2="49.5" y2="361" stroke="black" />
+      <line x1="49.5" y1="358" x2="49.5" y2="350" stroke="black" />
+      <line x1="49.5" y1="347" x2="49.5" y2="339" stroke="black" />
+      <line x1="49.5" y1="336" x2="49.5" y2="328" stroke="black" />
+    </svg>
+  );
+};
+
+const BottomPattern = () => {
+  return (
+    <svg
+      width="100"
+      height="485"
+      viewBox="0 0 100 485"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="50"
+        cy="482"
+        r="2.5"
+        transform="rotate(-180 50 482)"
+        fill="black"
+        stroke="black"
+      />
+      <path
+        d="M50 482L42.8904 467.394C16.1165 412.391 18.7879 347.612 50 295V295"
+        stroke="black"
+      />
+      <path
+        d="M50 295L57.1096 280.394C83.8835 225.391 81.2121 160.612 50 108V108"
+        stroke="black"
+      />
+      <circle
+        cx="50"
+        cy="295"
+        r="2.5"
+        transform="rotate(-180 50 295)"
+        fill="white"
+        stroke="black"
+      />
+      <circle
+        cx="50"
+        cy="108"
+        r="2.5"
+        transform="rotate(-180 50 108)"
+        fill="black"
+        stroke="black"
+      />
+      <path d="M50 108L50 55" stroke="black" />
+      <path d="M50 6L50 7.15256e-07" stroke="black" />
+      <path d="M50 50L50 44" stroke="black" />
+      <path d="M50 39L50 33" stroke="black" />
+      <path d="M50 28L50 22" stroke="black" />
+      <path d="M50 17L50 11" stroke="black" />
+    </svg>
+  );
+};
+
+const SocialsDesign = () => {
+  return (
+    <svg
+      width="61"
+      height="6"
+      viewBox="0 0 61 6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M25 3H61" stroke="black" />
+      <circle cx="58" cy="3" r="2.5" fill="black" stroke="black" />
+      <path d="M0 3H6" stroke="black" />
+      <path d="M8 3H14" stroke="black" />
+      <path d="M16 3H22" stroke="black" />
+    </svg>
+  );
+};
+
+const TwitterIcon = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.052 30C22.128 30 28.734 19.994 28.734 11.332C28.734 11.052 28.734 10.768 28.722 10.488C30.0081 9.55698 31.1181 8.40432 32 7.08401C30.7986 7.61371 29.5255 7.96282 28.222 8.12001C29.5951 7.29929 30.6234 6.00795 31.116 4.48601C29.826 5.25026 28.414 5.78676 26.942 6.07201C25.9524 5.01806 24.6428 4.31985 23.2162 4.08553C21.7895 3.85121 20.3254 4.09386 19.0507 4.77589C17.7759 5.45792 16.7616 6.54127 16.1649 7.85813C15.5682 9.175 15.4224 10.6519 15.75 12.06C13.1395 11.9291 10.5856 11.2509 8.25409 10.0695C5.92254 8.88805 3.86537 7.2297 2.216 5.20201C1.37868 6.64818 1.12315 8.35881 1.50129 9.98654C1.87944 11.6143 2.86291 13.0371 4.252 13.966C3.21108 13.9306 2.19304 13.6511 1.28 13.15V13.24C1.28179 14.755 1.80661 16.2229 2.76575 17.3955C3.72488 18.5682 5.0595 19.3738 6.544 19.676C5.98053 19.8312 5.39846 19.9086 4.814 19.906C4.40197 19.9073 3.99076 19.8691 3.586 19.792C4.00557 21.0961 4.82255 22.2363 5.92249 23.053C7.02244 23.8696 8.35027 24.3217 9.72 24.346C7.39311 26.1736 4.5188 27.1648 1.56 27.16C1.03864 27.1622 0.517646 27.1322 0 27.07C3.00303 28.9845 6.4906 30.0011 10.052 30Z"
+        fill="black"
+      />
+    </svg>
+  );
+};
+
+const GithubIcon = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_11_20)">
+        <path
+          d="M16 0C7.16 0 0 7.16 0 16C0 23.08 4.58 29.06 10.94 31.18C11.74 31.32 12.04 30.84 12.04 30.42C12.04 30.04 12.02 28.78 12.02 27.44C8 28.18 6.96 26.46 6.64 25.56C6.46 25.1 5.68 23.68 5 23.3C4.44 23 3.64 22.26 4.98 22.24C6.24 22.22 7.14 23.4 7.44 23.88C8.88 26.3 11.18 25.62 12.1 25.2C12.24 24.16 12.66 23.46 13.12 23.06C9.56 22.66 5.84 21.28 5.84 15.16C5.84 13.42 6.46 11.98 7.48 10.86C7.32 10.46 6.76 8.82 7.64 6.62C7.64 6.62 8.98 6.2 12.04 8.26C13.32 7.9 14.68 7.72 16.04 7.72C17.4 7.72 18.76 7.9 20.04 8.26C23.1 6.18 24.44 6.62 24.44 6.62C25.32 8.82 24.76 10.46 24.6 10.86C25.62 11.98 26.24 13.4 26.24 15.16C26.24 21.3 22.5 22.66 18.94 23.06C19.52 23.56 20.02 24.52 20.02 26.02C20.02 28.16 20 29.88 20 30.42C20 30.84 20.3 31.34 21.1 31.18C24.2765 30.108 27.0367 28.0667 28.9921 25.3435C30.9474 22.6203 31.9994 19.3525 32 16C32 7.16 24.84 0 16 0Z"
+          fill="black"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_11_20">
+          <rect width="32" height="32" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+const LinkedinIcon = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_11_25)">
+        <path
+          d="M0 2.292C0 1.026 1.052 0 2.35 0H29.65C30.948 0 32 1.026 32 2.292V29.708C32 30.974 30.948 32 29.65 32H2.35C1.052 32 0 30.974 0 29.708V2.292ZM9.886 26.788V12.338H5.084V26.788H9.886ZM7.486 10.364C9.16 10.364 10.202 9.256 10.202 7.868C10.172 6.45 9.162 5.372 7.518 5.372C5.874 5.372 4.8 6.452 4.8 7.868C4.8 9.256 5.842 10.364 7.454 10.364H7.486ZM17.302 26.788V18.718C17.302 18.286 17.334 17.854 17.462 17.546C17.808 16.684 18.598 15.79 19.926 15.79C21.664 15.79 22.358 17.114 22.358 19.058V26.788H27.16V18.5C27.16 14.06 24.792 11.996 21.632 11.996C19.084 11.996 17.942 13.396 17.302 14.382V14.432H17.27C17.2806 14.4153 17.2913 14.3986 17.302 14.382V12.338H12.502C12.562 13.694 12.502 26.788 12.502 26.788H17.302Z"
+          fill="black"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_11_25">
+          <rect width="32" height="32" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+const Star = () => {
+  return (
+    <svg
+      width="460"
+      height="482"
+      viewBox="0 0 460 482"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="230.5" cy="241.5" r="137" stroke="#E1E1E1" stroke-width="3" />
+      <path
+        d="M79.3786 328.75L230.5 67L381.621 328.75H79.3786Z"
+        stroke="#E1E1E1"
+        stroke-width="3"
+      />
+      <path
+        d="M79.3592 154.239L381.641 154.239L230.5 416.022L79.3592 154.239Z"
+        fill="#F6F6F6"
+        stroke="#E1E1E1"
+        stroke-width="3"
+      />
+    </svg>
+  );
+};
+
+const ArrowDown = () => {
+  return (
+    <svg
+      width="13"
+      height="57"
+      viewBox="0 0 13 57"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M6.5 57L0.00480938 46.5H12.9952L6.5 57Z" fill="black" />
+      <line x1="6.5" y1="46" x2="6.5" y2="30" stroke="black" />
+      <line x1="6.5" y1="17" x2="6.5" y2="10" stroke="black" />
+      <line x1="6.5" y1="27" x2="6.5" y2="20" stroke="black" />
+      <line x1="6.5" y1="7" x2="6.5" stroke="black" />
+    </svg>
+  );
+};
+
+const ArrowRight = () => {
+  return (
+    <svg
+      width="15"
+      height="18"
+      viewBox="0 0 15 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.5 1.20577L14 9L0.5 16.7942L0.5 1.20577Z"
+        fill="#F5F5F5"
+        stroke="black"
+      />
+    </svg>
+  );
+};
+
+const ProjectsDesign = () => {
+  return (
+    <svg
+      width="314"
+      height="1"
+      viewBox="0 0 314 1"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line x1="169" y1="0.5" x2="314" y2="0.5" stroke="black" />
+      <line y1="0.5" x2="15" y2="0.5" stroke="black" />
+      <line x1="22" y1="0.5" x2="37" y2="0.5" stroke="black" />
+      <line x1="43" y1="0.5" x2="58" y2="0.5" stroke="black" />
+      <line x1="64" y1="0.5" x2="79" y2="0.5" stroke="black" />
+      <line x1="85" y1="0.5" x2="100" y2="0.5" stroke="black" />
+      <line x1="106" y1="0.5" x2="121" y2="0.5" stroke="black" />
+      <line x1="127" y1="0.5" x2="142" y2="0.5" stroke="black" />
+      <line x1="148" y1="0.5" x2="163" y2="0.5" stroke="black" />
+    </svg>
+  );
+};
+
+export const Icons = ({ name }) => {
+  const icons = {
+    designTop: TopPattern,
+    designMiddle: MiddlePattern,
+    designBottom: BottomPattern,
+    designSocials: SocialsDesign,
+    designProjects: ProjectsDesign,
+    twitter: TwitterIcon,
+    github: GithubIcon,
+    linkedin: LinkedinIcon,
+    star: Star,
+    arrowDown: ArrowDown,
+    arrowRight: ArrowRight,
+  };
+
+  const Component = icons[name];
+
+  return <Component />;
+};
