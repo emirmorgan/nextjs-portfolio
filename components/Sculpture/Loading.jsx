cc74f423da98768d2572c169fb039d8c13aa0b1a
emirmorgan <aemirdnr@gmail.com>
2022-12-24 20:00:42 +0300

    Update: Loader animation

--- components/Sculpture/Loading.jsx
+++ components/Sculpture/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="three_loading">
+    <div className="flex items-center justify-center w-full h-full">
       <span className="loader" />
     </div>
   );
