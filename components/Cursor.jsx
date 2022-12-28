e6656bf363df2ad25e38f328fd0b8f03b9f8047a
emirmorgan <aemirdnr@gmail.com>
2022-12-28 03:14:40 +0300

    Fix: cursor effect on mobile

--- components/Cursor.jsx
+++ components/Cursor.jsx
@@ -14,7 +14,7 @@ const Cursor = () => {
       style={{
         left: location.x,
         top: location.y,
-        display: location.x === 0 ? "none" : "block",
+        visibility: location.x === 0 ? "hidden" : "visible",
       }}
     ></div>
   );
